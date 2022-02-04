037b8d5f33e732fb2e7e2cce1ebf50da473f2a22
Tonivan Joseph <37024453+tonivan-joseph@users.noreply.github.com>
2022-02-03 23:18:29 -0300

    Esta sendo implementado o formulário de edição do usário do sistrama, é o formulário de edição do usário para fazer alteração necessários.

---
+++ update_user.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Atualizar Usuarios</title>
+</head>
+<body>
+    <?php 
+         require_once 'config.php';
+         require_once 'models/produtos.php';
+         require_once 'dataAcessObject/ProdutosMysql.php';
+ 
+         // instanciando o objeto usuarioDAO
+         $updateUser = new ProdutosMysql($pdo);
+        
+        $infoUser = false;//variável vai ter a informação do produto
+        $id = filter_input(INPUT_GET, 'id');
+
+        if($id) {
+            //Aqui é instanciado um valor do id se ele achar um id ele vai criar um objeto desse id caso contrário ele vai ser uma instância false.
+            $infoUser = $updateUSer->findById($id);
+        }
+        //Se a informação do id for falso, ele reencaminhar para o index.ph do sistema, caso ele for verdadeiro ai ele mostra os fomulários ja preenchidos.
+        if($infoUser === false ){
+            // header("Location: index.php");
+        }
+    ?>
+    <h1>Formulários de Atualização dos usuários</h1>
+    
+    <section class="section_add_product">
+        <h3>Editar Usuários</h3>
+        
+        <form class="form_user" action="update_action_user.php" method="post">
+            <!-- campo oculto -->
+            <input type="hidden" name="id" value="<?=$infoUser->getId();?>">
+
+            <div class="inputBox">
+                <label id="nome" class="labelIput">Editar Nome:</label>
+                <input type="number" name="nome" id="nome" class="iputUser" value="<?=$infoUser->getNome();?>">
+            </div>
+
+            <div class="inputBox">
+                <label id="nome" class="labelIput">Editar E-mail:</label>
+                <input type="email" name="email" id="email" class="iputUser" value="<?=$infoUser->getEmail();?>">
+            </div>
+
+            <div class="inputBox">
+                <label id="preco" class="labelIput">Editar Senha:</label>
+                <br/>
+                <input type="text" name="senha" id="senha" class="iputUser" value="<?=$infoUser->getSenha();?>">
+            </div>
+            <br>
+            <input class="iputButton" type="submit" name="submit" id ="submit" placehold="Atualizar">
+            <div class="btn-index"><a href="index.php">Voltar Home</a></div>
+    </section>    
+</body>
+</html>
