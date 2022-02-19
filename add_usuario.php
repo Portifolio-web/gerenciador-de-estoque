dd68342eaf38359067bf7a5e2e8699745d8504bf
Tonivan Joseph <37024453+tonivan-joseph@users.noreply.github.com>
2022-02-19 11:42:54 -0300

    Foi adicionado a página de inserir usários que é apontado na secção menu lateral, onde dentro da página usários temos todos os dados necessários de um usuário para o sistema.

---
+++ add_usuario.php
@@ -0,0 +1,67 @@
+<?php 
+session_start();
+?>
+    <?php 
+        require_once 'header.php';
+        require_once 'menu_lateral.php';
+    ?>
+    
+    <section class="section_add_product">
+    
+        <div class="alert">
+            <?php 
+                if($_SESSION['alert'] != '') {
+                    echo "<p>".$_SESSION['alert']."</p>";
+                    $_SESSION['alert']='';
+                }else if($_SESSION['alert'] != '') {
+                    echo "<p>".$_SESSION['alert']."</p>";
+                    $_SESSION['alert']='';
+                }else {
+                    echo "<p>".$_SESSION['alert']."</p>";
+                    $_SESSION['alert']=''; 
+                }
+            ?>
+        </div> 
+        <h3>Cadastre Um Usuário Aqui.</h3>
+
+        <form class="form_user" action="add_product_action.php" method="post">
+            
+            <div class="inputBox">
+                <label id="nome" class="labelIput">Nome:</label>
+                <input type="text" name="nome" id="nome" class="iputUser">
+            </div>
+
+            <div class="inputBox">
+                <label id="email" class="labelIput">E-mail:</label>
+                <input type="email" name="email" id="email" class="iputUser" >
+            </div>
+
+            <div class="inputBox">
+                <label id="senha" class="labelIput">Senha:</label>
+                <br/>
+                <input type="password" name="cnpj" id="cnpj" class="iputUser">
+            </div>
+            <h3>Endereço:</h3>    
+            <div class="inputBox">
+                <label id="cidade" class="labelIput">Cidade:</label>
+                <input type="text" name="cidade" id="cidade" class="iputUser">
+            </div>
+
+            <div class="inputBox">
+                <label id="estado" class="labelIput">Estado:</label>
+                <input type="text" name="estado" id="estado" class="iputUser">
+            </div>
+            <div class="inputBox">
+                <label id="rua" class="labelIput">Rua:</label>
+                <input type="text" name="rua" id="rua" class="iputUser">
+            </div>
+            <div class="inputBox">
+                <label id="cep" class="labelIput">CEP:</label>
+                <input type="number" name="cep" id="cep" class="iputUser">
+            </div>
+            <br>
+            <input class="iputButton" type="submit" name="submit" value= "Cadastrar "id ="submit">
+    </section> 
+      
+</body>
+</html>
--- menu_lateral.php
+++ menu_lateral.php
@@ -6,7 +6,7 @@
                 <li class="btn" id="cadastro">
                     <a href="#cadastro"><i class="fa-solid fa-square-plus"></i>Cadastros</a>
                     <div class="smenu">
-                        <a href="add_user.php"><i class="fa-solid fa-user-plus"></i>Usuários</a>
+                        <a href="add_usuario.php"><i class="fa-solid fa-user-plus"></i>Usuários</a>
                         <a href="add_fornecedor.php"><i class="fa-solid fa-bag-shopping"></i>Fornecedores</a>
                         <a href="add_product.php"><i class="fa-solid fa-money-check-dollar"></i>Produtos</a>
                     </div>
